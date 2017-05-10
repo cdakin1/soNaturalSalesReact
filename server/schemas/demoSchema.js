279e4c6a352fa569c83c50ae6de5ee1158b97b12
Christopher Dakin <christopherdakin1@gmail.com>
2017-05-10 18:28:23 -0500

    began implementing graphql server

---
+++ server/schemas/demoSchema.js
@@ -0,0 +1,22 @@
+const graphqlTools = require('graphql-tools')
+
+var module = { exports: {} };
+
+const typeDefs = `
+type Demo {
+   id: ID!                # "!" denotes a required field
+   name: String,
+   date: String,
+   store: String,
+   line: String,
+   location: String
+}
+# This type specifies the entry points into our API. In this case
+# there is only one - "channels" - which returns a list of channels.
+type Query {
+   demos: [Demo]    # "[]" means this is a list of channels
+}
+`;
+
+exports.schema = graphqlTools.makeExecutableSchema({ typeDefs });
+return module.exports;
